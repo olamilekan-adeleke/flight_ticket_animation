8b4a2d31852f5bc5f2643d803a9a4817207d7c77
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-11-17 03:21:33 +0100

    update

--- lib/widgets/flight_widget/flight_details_widget.dart
+++ lib/widgets/flight_widget/flight_details_widget.dart
@@ -5,8 +5,35 @@ import 'package:flutter_svg/flutter_svg.dart';
 import '../dash_line_widget.dart';
 import '../sizer_box_helper.dart';
 
-class TravelDetailsWidget extends StatelessWidget {
+class TravelDetailsWidget extends StatefulWidget {
   const TravelDetailsWidget({Key? key}) : super(key: key);
+
+  @override
+  State<TravelDetailsWidget> createState() => _TravelDetailsWidgetState();
+}
+
+class _TravelDetailsWidgetState extends State<TravelDetailsWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 600),
+  );
+
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fadeAnimation = CurvedAnimation(
+      curve: Curves.easeIn,
+      parent: _animationController,
+    );
+
+    Future.delayed(const Duration(milliseconds: 900)).then(((value) {
+      _animationController.forward();
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,25 +62,57 @@ class TravelDetailsWidget extends StatelessWidget {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        locationTextWidget('ABV', 'Abuja'),
+                        FadeTransition(
+                          opacity: _fadeAnimation,
+                          child: locationTextWidget('ABV', 'Abuja'),
+                        ),
                         flightPathIcon(),
-                        locationTextWidget('LHR', 'London'),
+                        FadeTransition(
+                          opacity: _fadeAnimation,
+                          child: locationTextWidget('LHR', 'London'),
+                        ),
                       ],
                     ),
                     const H(height: 15),
                     Row(
                       children: [
-                        boardingTextWidget('Boarding Date', 'Aug 12, 2022'),
-                        boardingTextWidget('Boarding Time', '9:30am', true),
+                        FadeTransition(
+                          opacity: _fadeAnimation,
+                          child: boardingTextWidget(
+                            'Boarding Date',
+                            'Aug 12, 2022',
+                          ),
+                        ),
+                        FadeTransition(
+                          opacity: _fadeAnimation,
+                          child: boardingTextWidget(
+                            'Boarding Time',
+                            '9:30am',
+                            true,
+                          ),
+                        ),
                       ],
                     ),
                     const H(height: 15),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        boardingTextWidget('Gate', 'J22'),
-                        boardingTextWidget('Seat', '19J'),
-                        boardingTextWidget('Flight No.', 'DN2903'),
-                        boardingTextWidget('Luggage', '3 Y'),
+                        FadeTransition(
+                          opacity: _fadeAnimation,
+                          child: boardingTextWidget('Gate', 'J22'),
+                        ),
+                        FadeTransition(
+                          opacity: _fadeAnimation,
+                          child: boardingTextWidget('Seat', '19J'),
+                        ),
+                        FadeTransition(
+                          opacity: _fadeAnimation,
+                          child: boardingTextWidget('Flight No.', 'DN2903'),
+                        ),
+                        FadeTransition(
+                          opacity: _fadeAnimation,
+                          child: boardingTextWidget('Luggage', '3 Y'),
+                        ),
                       ],
                     ),
                     const H(height: 20),
