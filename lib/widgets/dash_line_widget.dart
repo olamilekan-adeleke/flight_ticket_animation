b7561ed76c61cc9622ad5ba0d1f6e47bccbcbe01
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-11-17 03:17:02 +0100

    update

--- lib/widgets/dash_line_widget.dart
+++ lib/widgets/dash_line_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class DashedLine extends StatelessWidget {
+class DashedLine extends StatefulWidget {
   final double height;
   final double heightContainer;
   final Color color;
@@ -17,34 +17,69 @@ class DashedLine extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: direction == Axis.vertical ? heightContainer - 0.8 : 10,
-      child: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          final boxHeight = constraints.constrainHeight();
-          final dashHeight = height;
-          final dashCount = direction == Axis.vertical
-              ? (boxHeight / (2 * dashHeight)).floor()
-              : 18;
+  State<DashedLine> createState() => _DashedLineState();
+}
 
-          return Flex(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            direction: direction,
-            children: List.generate(
-              dashCount,
-              (_) {
-                return SizedBox(
-                  width: dashWidth,
-                  height: dashHeight,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(color: color),
-                  ),
-                );
-              },
-            ),
-          );
-        },
+class _DashedLineState extends State<DashedLine>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 600),
+  );
+
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+
+    startAnimation();
+  }
+
+  Future<void> startAnimation() async {
+    await Future.delayed(const Duration(milliseconds: 800));
+
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _animation,
+      child: SizedBox(
+        height: widget.direction == Axis.vertical
+            ? widget.heightContainer - 0.8
+            : 10,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            final boxHeight = constraints.constrainHeight();
+            final dashHeight = widget.height;
+            final dashCount = widget.direction == Axis.vertical
+                ? (boxHeight / (2 * dashHeight)).floor()
+                : 18;
+
+            return Flex(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              direction: widget.direction,
+              children: List.generate(
+                dashCount,
+                (_) {
+                  return SizedBox(
+                    width: widget.dashWidth,
+                    height: dashHeight,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(color: widget.color),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/widgets/flight_widget/travel_title_and_logo_widget.dart
+++ lib/widgets/flight_widget/travel_title_and_logo_widget.dart
@@ -3,35 +3,80 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../sizer_box_helper.dart';
 
-class TravelTitleAndLogoWidget extends StatelessWidget {
+class TravelTitleAndLogoWidget extends StatefulWidget {
   const TravelTitleAndLogoWidget({Key? key}) : super(key: key);
 
   @override
+  State<TravelTitleAndLogoWidget> createState() =>
+      _TravelTitleAndLogoWidgetState();
+}
+
+class _TravelTitleAndLogoWidgetState extends State<TravelTitleAndLogoWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 600),
+  );
+
+  late Animation<Offset> _offsetAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(0.5, 0),
+      end: const Offset(0, 0),
+    ).animate(
+      CurvedAnimation(curve: Curves.easeIn, parent: _animationController),
+    );
+
+    _fadeAnimation = CurvedAnimation(
+      curve: Curves.easeIn,
+      parent: _animationController,
+    );
+
+    Future.delayed(const Duration(milliseconds: 900)).then(((value) {
+      _animationController.forward();
+    }));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Image.asset('assets/logo.png', height: 42.h, width: 200.w),
-        const H(height: 20),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const <Widget>[
-            Text(
-              'Name',
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                color: Color(0xff667085),
-              ),
+    return FadeTransition(
+      opacity: _fadeAnimation,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SlideTransition(
+            position: _offsetAnimation,
+            child: Image.asset('assets/logo.png', height: 42.h, width: 200.w),
+          ),
+          const H(height: 20),
+          SlideTransition(
+            position: _offsetAnimation,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                Text(
+                  'Name',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xff667085),
+                  ),
+                ),
+                Text(
+                  'Enigma Kod',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                ),
+              ],
             ),
-            Text(
-              'Enigma Kod',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-            ),
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
